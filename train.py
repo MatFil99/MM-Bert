4b42001c2bac522dc583d951d05b7f185158ee89
Filip <filip.mazur123@gmail.com>
2024-04-30 23:34:16 +0200

    Training loop, evaluation function
    First versions of training loop, evaluation function. Added saving and
    reading aligned dataset.

--- train.py
+++ train.py
@@ -1,4 +1,5 @@
 from collections import defaultdict
+import copy 
 
 import torch
 from torch import nn
@@ -7,6 +8,7 @@ from torch.optim import AdamW
 import transformers
 from transformers import get_scheduler, AutoTokenizer, DataCollatorWithPadding, DefaultDataCollator
 from tqdm.auto import tqdm
+import evaluate
 
 import cmbert
 from mmsdk.mmdatasdk.dataset.standard_datasets.CMU_MOSI import cmu_mosi_std_folds as standard_folds
@@ -20,10 +22,6 @@ def print_batch(batch):
     print(f"last batch: {batch.keys()}")
     print(f"labels cuda: {batch['labels'].is_cuda}")
 
-def train(model, tokenizer, ds):
-    seed = 7
-    transformers.set_seed(seed)
-    torch.manual_seed(seed)
 
 class MultiModalDataCollator():
 
@@ -39,16 +37,49 @@ class MultiModalDataCollator():
         tokenized['labels'] = torch.tensor(batch['labels'], dtype=torch.long)
         tokenized.to(self.device)
         return  tokenized
+
+def evaluation(model, dataloader):
+    metrics_names = ['accuracy', 'f1']
+    metrics = {metric: evaluate.load(metric) for metric in metrics_names}
+
+    model.eval()
+    vloss = 0.0
+    # with torch.no_grad():
+    for batch in dataloader:
+        del batch['visual_data']
+        logits, text_att, fusion_att = model(**batch)
+
+        vloss += criterion(logits, batch['labels']).item()
+
+        predictions = torch.argmax(logits, dim=-1)
+
+        for name, metric in metrics.items():
+            metric.add_batch(predictions=predictions, references=batch['labels'])
     
+    calculated_metrics = {}
+    for _, metric in metrics.items():
+        calculated_metrics.update(metric.compute())
+    calculated_metrics['loss'] = vloss
+
+    return calculated_metrics
+
 def prepare_data_splits(ds):
     folds = {
-        'train': ['03bSnISJMiM', '0h-zjBukYpk', '1DmNV9C1hbY', '1iG0909rllw', '2WGyTLYerpo'], # standard_folds.standard_train_fold,
-        'valid': ['2iD-tVS8NPw', '5W7Z1C_fDaE', '6Egk_28TtTM'], # standard_folds.standard_valid_fold,
-        'test': ['6_0THN4chvY', '73jzhE8R1TQ']# standard_folds.standard_test_fold
+        'train': standard_folds.standard_train_fold,
+        'valid': standard_folds.standard_valid_fold,
+        'test': standard_folds.standard_test_fold,
+        # 'train': ['03bSnISJMiM', '0h-zjBukYpk', '1DmNV9C1hbY', '1iG0909rllw', '2WGyTLYerpo'], # standard_folds.standard_train_fold,
+        # 'valid': ['2iD-tVS8NPw', '5W7Z1C_fDaE', '6Egk_28TtTM'], # standard_folds.standard_valid_fold,
+        # 'test': ['6_0THN4chvY', '73jzhE8R1TQ']# standard_folds.standard_test_fold
         }
     ds.train_test_valid_split(folds=folds)
     
     ds.computational_sequences_2_array()
+
+    # print(ds.dataset.keys())
+    # print(ds.dataset['train'].keys())
+    # print(ds.dataset['train']['text_feat'][0])
+    
     ds.words_2_sentences()
 
     train_ds = cmbert.CmuDataset.from_dataset(ds, fold='train')
@@ -62,6 +93,9 @@ def train(model, train_dataloader, valid_dataloader, num_epochs, optimizer, lr_s
     num_training_steps = num_epochs * len(train_dataloader)
     progress_bar = tqdm(range(num_training_steps))
     train_loss = []
+
+    best_model = copy.deepcopy(model)
+    best_accuracy = 0.0
     
     for epoch in range(num_epochs):
         model.train()
@@ -78,21 +112,39 @@ def train(model, train_dataloader, valid_dataloader, num_epochs, optimizer, lr_s
             optimizer.step()
             lr_scheduler.step()
             optimizer.zero_grad()
-            
+            progress_bar.update(1)
+        
+        valid_evaluation = evaluation(model, valid_dataloader)
         print('==================================')
         print(f'Epoch {epoch + 1} / {num_epochs}')
         print(f'Training loss: {cum_loss}')
-        # print('Validation: ', evaluation)
-
+        print('Validation: ', valid_evaluation)
+        if valid_evaluation['accuracy'] > best_accuracy:
+            best_model = copy.deepcopy(model)
+            best_accuracy = valid_evaluation['accuracy']
+        
+    return best_model
 
 if __name__ == '__main__':
+    seed = 7
+    transformers.set_seed(seed)
+    torch.manual_seed(seed)
+
     print("train.py")
 
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
 
+    load_preprocessed = True
+
     # dataset configuration
-    dataset_config = cmbert.DataConfig()
-    
+    if load_preprocessed:
+        dataset_config = cmbert.DataConfig(
+            ds_path=r'D:\Studia\magisterskie\Praca_magisterska\data\repo\CMU-MultimodalSDK\cmumosi\aligned',        
+            load_preprocessed=True        
+            )
+    else:
+        dataset_config = cmbert.DataConfig()
+
     # model configuration
     checkpoint = 'google-bert/bert-base-uncased'
     model_config = cmbert.CMBertConfig()
@@ -100,36 +152,30 @@ if __name__ == '__main__':
 
 
     # create dataset, model, tokenizer
-    ds = cmbert.CmuDataset(dataset_config)
+    
+    if load_preprocessed:
+        ds = cmbert.CmuDataset(dataset_config, preprocess=False)
+    else:
+        ds = cmbert.CmuDataset(dataset_config, preprocess=True)
+
     model = cmbert.CMBertForSequenceClassification(config=model_config, num_labels=num_labels)
     # tokenizer = cmbert.MultimodalTokenizer(checkpoint=checkpoint)
+
+    # save data
+    # ds.deploy()
+    
+    if load_preprocessed:
+        ds.append_labels_to_dataset()
+    
+    ds.labels_2_class(num_classes=num_labels)
 
     # prepare data
     train_ds, valid_ds, test_ds = prepare_data_splits(ds=ds)
     
 
-    # # select sample data
-    # n = 10
-    # # text = text[:n]
-    # # audio_feat = features['audio_feat'][:n]
-    # # visual_feat = features['visual_feat'][:n]
-
-    # text = data['train']['features']['text_feat'][:n]
-    # audio_feat = data['train']['features']['audio_feat'][:n]
-    # visual_feat = data['train']['features']['visual_feat'][:n]
-
-    # tokenized = tokenizer(text=text, audio=audio_feat, visual=visual_feat, padding=True, truncation=True)
-
-    # # # use all data
-    # # tokenized = tokenizer(text=text, audio=features['audio_feat'], visual=features['visual_feat'], padding=True, truncation=True)
-
-    # # remove visual features
-    # del tokenized['visual_data']
-
-
-    # 
+    # # # 
     criterion = nn.CrossEntropyLoss()
-    optimizer = AdamW(model.parameters(), lr=5e-5)
+    optimizer = AdamW(model.parameters(), lr=1e-3)
     device = torch.device("cuda") if torch.cuda.is_available() else torch.device("cpu")
     model.to(device)
     criterion.to(device)
@@ -139,6 +185,7 @@ if __name__ == '__main__':
 
     train_dataloader = DataLoader(train_ds, batch_size=4, collate_fn=data_collator)
     valid_dataloader = DataLoader(valid_ds, batch_size=4, collate_fn=data_collator)
+    test_dataloader = DataLoader(test_ds, batch_size=4, collate_fn=data_collator)
 
     num_epochs = 3
     num_training_steps = num_epochs*len(train_dataloader)
@@ -158,13 +205,15 @@ if __name__ == '__main__':
         # print(f'logits: {logits}')
         # print(f'text_att: {text_att.shape}')
         # print(f'fusion_att: {fusion_att.shape}')
+
+    
         
-        # break
-    # print(i)
-    # print(sentences)
+    #     # break
+    # # print(i)
+    # # print(sentences)
 
 
-    train(
+    best_model = train(
         model=model,
         train_dataloader=train_dataloader,
         valid_dataloader=valid_dataloader,
@@ -174,37 +223,6 @@ if __name__ == '__main__':
         criterion=criterion,
     )
 
-    # text = tokenized['input_ids'].to(device)
-    # tokenized['audio_data'].to(device)
-    
-    # print(tokenized['input_ids'].is_cuda)
-    # print(tokenized['audio_data'].is_cuda)
+    calculated_metrics = evaluation(best_model, test_dataloader)
 
-
-
-    # data_collator = DefaultDataCollator()
-    # # data_collator = DataCollatorWithPadding(tokenizer=tokenizer)
-    # train_dataloader = DataLoader(
-    #     tokenized, shuffle=True, batch_size=4, collate_fn=data_collator
-    # )
-
-    # print(train_dataloader)
-
-    # for batch in train_dataloader:
-    #     print(batch)
-
-    # num_epochs = 3
-    # num_training_steps = num_epochs * len(train_dataloader)
-    # lr_scheduler = get_scheduler(
-    #     "linear",
-    #     optimizer=optimizer,
-    #     num_warmup_steps=0,
-    #     num_training_steps=num_training_steps,
-    # )
-
-    # logits, text_att, fusion_att = model(**tokenized)
-    # print(f'logits: {logits}')
-    # print(f'text_att: {text_att.shape}')
-    # print(f'fusion_att: {fusion_att.shape}')
-    
-    # print(f'labels: {data["valid"]["labels"][:2]}')
+    print(calculated_metrics)
